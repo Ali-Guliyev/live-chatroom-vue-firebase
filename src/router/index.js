af5e2edbf8d70d1b20da992d4dd894c792f56baa
Ali Guliyev <ali.guliyevoo7@mail.com>
2021-07-27 19:28:54 +0300

    nothing

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Welcome from "../views/Welcome.vue";
 import Chatroom from "../views/Chatroom.vue";
 import { projectAuth } from "../firebase/config";
 
-// auth guard
+// auth guards
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser;
+  console.log("current user in auth guard: ", user);
   if (!user) {
     next({ name: "Welcome" });
   } else {
