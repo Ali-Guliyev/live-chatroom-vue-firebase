9cc5287182a9e850f8fa2daf0564b2318a4b1fe0
Ali Guliyev <ali.guliyevoo7@mail.com>
2021-07-27 18:52:29 +0300

    updated logo and the name of app

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="welcome container">
-    <p>Welcome</p>
+    <p>Welcome to the <b>Chatroom</b></p>
     <div v-if="showLogin">
       <h2>Login</h2>
       <LoginForm @login="enterChat" />
